ab69598f44fc336c11aa49b27e49f9b5ea97a772
Kira9112 <kira91_911@mail.ru>
2021-07-14 15:35:33 +0300

    update StatisticsService

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -9,7 +9,7 @@ public class StatisticsService {
      */
     public long findMax(long[] incomes) {
         long current_max_index = 0;
-        long current_max = incomes[0];
+        long current_max = incomes[1];
         for (long income : incomes)
             if (current_max < income)
                 current_max = income;
