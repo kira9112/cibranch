a4e3b25e6ca9953f49bf83658f366f7a6f91f043
Kira9112 <kira91_911@mail.ru>
2021-07-19 15:39:08 +0300

    Update StatisticsServiceTest.java

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -18,4 +18,17 @@ class StatisticsServiceTest {
 
         assertEquals(12, actual);
     }
+
+    @Test
+    void findMax15() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 15};
+        long expected = 15;
+        long actual = service.findMax(incomesInBillions);
+
+        assertEquals(expected, actual);
+    }
+
+
 }
